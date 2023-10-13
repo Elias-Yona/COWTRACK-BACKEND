95f0d66175552d67656f1501bb8c3795e39c217f
Elias Yona <iconelias001@gmail.com>
2023-10-13 08:30:23 +0300

    chore: Add a view for viewing a Customer object

--- salesmetrics/views.py
+++ salesmetrics/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Customer
+from .serializers import CustomerSerializer
+
+
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
