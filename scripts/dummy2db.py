acec281de5f64fad15cd6b885b540f0dde3f9a8d
Elias Yona <iconelias001@gmail.com>
2023-10-14 05:53:17 +0300

    feat: Add logging information when starting up MySQL

--- scripts/dummy2db.py
+++ scripts/dummy2db.py
@@ -1,11 +1,21 @@
 import subprocess
 import time
 
+from .helpers import dummy2db_logger
+
+
+logger, extra_information = dummy2db_logger()
+
+
 def _mysqld_process_checkpoint():
     '''checks if mysql is available. if not it starts one
     '''
     try:
         subprocess.check_output("pgrep mysqld", shell=True)
     except Exception:
+        logger.warning(
+            'Your mysql server is offline, dummy2db will try to launch it now!',
+            extra=extra_information)
+
         subprocess.Popen("mysqld", close_fds=True, shell=True)
         time.sleep(3)
